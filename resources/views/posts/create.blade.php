d874df665891eaab49a30fa304ed33aa53456dc5
IzIzKen <111420knt@gmail.com>
2021-11-11 07:33:59 +0900

    211110_question

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,21 +4,32 @@
 
 @section('content')
 
-<h1>New Post</h1>
+    <h1>New Post</h1>
 
-<form mathod="POST" action="/posts">
-    {{ csrf_field( )}}
-    <div class="form-group">
-        <label for="exampleInputEmail1">Title</label>
-        <input type="text" class="form-control" aria-describedby="emailHelp" name="title">
-    </div>
-    <div class="form-group">
-        <label for="exampleInputPassword1">Content</label>
-        <textarea class="form-control" name="content"></textarea>
-    </div>
-    <button type="submit" class="btn btn-outline-primary">Submit</button>
-</form>
 
-<a href="/posts">Back</a>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach (&errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form mathod="POST" action="/posts">
+        {{ csrf_field( )}}
+        <div class="form-group">
+            <label for="exampleInputEmail1">Title</label>
+            <input type="text" class="form-control" aria-describedby="emailHelp" name="title" value="{{old('title')}}">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Content</label>
+            <textarea class="form-control" name="content">{{old('content')}}</textarea>
+        </div>
+        <button type="submit" class="btn btn-outline-primary">Submit</button>
+    </form>
+
+    <a href="/posts">Back</a>
 
 @endsection
